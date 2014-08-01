67c8426ef97ed8ec2b2eb1a6d5aaf043be92aa25
Charles Hans Thompson <hans.thompson1@gmail.com>
2014-08-01 15:42:32 -0800

    organize trails into systems

--- R/combine_trail_sources.R
+++ R/combine_trail_sources.R
@@ -30,18 +30,84 @@ head(anc_trails.df)
 chugach_trails.df <- chugach_trails.df[, colnames(chugach_trails.df) != "Description"]
 colnames(chugach_trails.df)[7] <- "TRAIL_NAME"
 all_trails <- rbind.fill(chugach_trails.df, anc_trails.df)
+unnamed_trails <- all_trails[all_trails$TRAIL_NAME &in& c("Unnamed", "Unnnamed"),]
 NA_trails <- all_trails[which((is.na(all_trails$TRAIL_NAME))),]
+NA_trails <- rbind(NA_trails, unnamed_trails)
 named_trails <- all_trails[which((!is.na(all_trails$TRAIL_NAME))),]
+named_trails <- named_trails[!named_trails$TRAIL_NAME %in% c("Unnamed", "Unnnamed"),]
+                                                        
+# Remove trails that are on roads (excluding highways). This is
+# just a judgement call that people don't care about trails that
+# follow city roads. 
+notes <- read.csv("doc/notes on the data to clean.csv")
+trails_to_remove <- notes[notes$first_notes == "weak trail",]$x
+named_trails <- droplevels(named_trails[!named_trails$TRAIL_NAME %in% trails_to_remove,])
+# Remove minitrails for now
+trails_to_remove <- notes[notes$first_notes == "minitrail",]$x
+named_trails <- droplevels(named_trails[!named_trails$TRAIL_NAME %in% trails_to_remove,])
+# Look at trails that don't fit in the box of googlemaps 
+use_bounding_box <- notes[notes$first_notes == "fix my plot to browse to zoom out",]$x
+bounding_box <- droplevels(named_trails[named_trails$TRAIL_NAME %in% use_bounding_box,])
 
-# browse trails
-for(i in seq(levels(named_trails$TRAIL_NAME))) {
-one_trail <- named_trails[which(named_trails$TRAIL_NAME == levels(named_trails$TRAIL_NAME)[i]),]
+# look up close at the trail
+for(i in seq(levels(bounding_box$TRAIL_NAME))) {
+    one_trail <- bounding_box[which(bounding_box$TRAIL_NAME == levels(bounding_box$TRAIL_NAME)[i]),]
+    map_center <- c(min(one_trail$long) - 0.04, min(one_trail$lat) - 0.04, max(one_trail$long) + 0.04, max(one_trail$lat) + 0.04)
+    anc_map <- get_map(map_center, source = "osm")
+    p <- ggmap(anc_map) +
+        geom_point(data = one_trail, aes(x = long, y = lat, color = TRAIL_NAME), size = 1) +
+        geom_line(data = one_trail, aes(x = long, y = lat, color = TRAIL_NAME, size = 1)) +
+        ggtitle(levels(bounding_box$TRAIL_NAME)[i]) +
+        theme(legend.position="none")
+    ggsave(paste(i, ".png", sep = ""))
+}
+write.csv(levels(bounding_box$TRAIL_NAME), file = "long trails to give a system.csv")
+
+# Group up the $SYSTEM_NAM into new $trail_system
+trail_system <- rep(NA, dim(named_trails)[1])
+trail_system[named_trails$SYSTEM_NAM %in% c("Kincaid Other Trails", "Kincaid Single Track", "Kincaid Ski Trails")] <- "Kincaid"
+trail_system[named_trails$SYSTEM_NAM %in% c("Girdwood Trails")] <- "Girdwood"
+trail_system[named_trails$SYSTEM_NAM %in% c("Johnson Trail")] <- "Seward Highway"
+trail_system[named_trails$SYSTEM_NAM %in% c("Rabbit Creek Greenbelt", "Ruth Arcand Trails")] <- "South Anchorage Hillside"
+trail_system[named_trails$SYSTEM_NAM %in% c("Powerline")] <- "Glen Alps"
+trail_system[named_trails$SYSTEM_NAM %in% c("Gasline Trail", "Abbot Trail", "Abbott Loop Trail", "Powerline", "Hillside Ski Trails", "Hillside Trail System", "Mt Bike Single Track")] <- "Bicentenial Park"
+trail_system[named_trails$SYSTEM_NAM %in% c("Mirror Lake Ski Trails", "Beach Lake Sled Dog", "Beech Lake Ski Trails")] <- "Peters Creek"
+trail_system[named_trails$SYSTEM_NAM %in% c("Russian Jack Ski Trails")] <- "UAA, APU, and Airport Heights"
+trail_system[named_trails$SYSTEM_NAM %in% c("Campbell Creek Trail")] <- "Campbell Creek"
+trail_system[named_trails$SYSTEM_NAM %in% c("Connors Bog Skijor")] <- "Connor's Bog"
+trail_system[named_trails$SYSTEM_NAM %in% c("Ship Creek Trail", "Coastal Trail")] <- "Coastal Trail"
+trail_system[named_trails$SYSTEM_NAM %in% c("Glenn Hwy Trail")] <- "Glenn Hwy"
+trail_system[named_trails$SYSTEM_NAM %in% c("Centennial Park Trails")] <- "Centenial Parks"
+trail_system[named_trails$SYSTEM_NAM %in% c("Bartlett Ski Trails")] <- "Bartlett"
+trail_system[named_trails$SYSTEM_NAM %in% c("Chester Creek Trail")] <- "Chester Creek"
+named_trails <- cbind(named_trails, trail_system)
+# Did I miss any (besides Tour de Anchorage)? Nope
+levels(droplevels(named_trails[is.na(named_trails$trail_system),]$SYSTEM_NAM))
+# Look at trails that aren't in a system yet
+no_category_yet <- droplevels(named_trails[is.na(trail_system),])
+levels(no_category_yet$TRAIL_NAME)
+
+
+# look up close at the trail
+for(i in seq(levels(no_category_yet$TRAIL_NAME))) {
+one_trail <- named_trails[which(no_category_yet$TRAIL_NAME == levels(named_trails$TRAIL_NAME)[i]),]
 map_center <- c(min(one_trail$long) - 0.02, min(one_trail$lat) - 0.02, max(one_trail$long) + 0.02, max(one_trail$lat) + 0.02)
 anc_map <- get_map(map_center)
 p <- ggmap(anc_map) +
-    geom_point(data = one_trail, aes(x = long, y = lat, color = TRAIL_NAME)) +
+    geom_point(data = one_trail, aes(x = long, y = lat, color = TRAIL_NAME), size = .01) +
+    geom_line(data = one_trail, aes(x = long, y = lat, color = TRAIL_NAME, size = .01)) +
     ggtitle(levels(named_trails$TRAIL_NAME)[i]) +
     theme(legend.position="none")
 ggsave(paste(i, ".png", sep = ""))
+Sys.sleep(4)
 }
 
+# look where in Anchorage the systems all ready exist
+anc_map <- get_map(c(lat = 61.2, lon = -149.76), zoom = 9)
+for(i in seq(levels(named_trails$TRAIL_NAME))) {
+ggmap(anc_map) +
+    geom_line(data = named_trails[named_trails$SYSTEM_NAM == levels(named_trails$SYSTEM_NAM)[i],],
+              aes(x = long, y = lat, color = TRAIL_NAME, group = TRAIL_NAME), size = .5) +
+    ggtitle(levels(named_trails$SYSTEM_NAM)[i]) +
+ggsave(paste("whichTrailSystem", i, ".png", sep = ""))
+}
